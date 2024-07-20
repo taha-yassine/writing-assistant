409896e6d55643e78cbeb75214e167cd0e03d34a
Taha YASSINE <yassinetaha1997@gmail.com>
2024-07-20 23:16:52 +0100

    Migrate to Vite.js

--- frontend/types/index.ts
+++
@@ -1,9 +0,0 @@
-export interface AnalysisResult {
-    grammarErrors: Array<{
-      message: string;
-      offset: number;
-      length: number;
-    }>;
-    tone: string;
-    suggestions: string[];
-  }
